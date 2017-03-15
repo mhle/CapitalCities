ac77d8fe25a021a7c8f28cf9171d50f284816325
Michael Le <hle.michael@gmail.com>
2017-03-15 23:52:57 +0000

    adding annotations

---
+++ CapitalCities/Capital.swift
@@ -0,0 +1,22 @@
+//
+//  Capital.swift
+//  CapitalCities
+//
+//  Created by Michael Le on 15/03/2017.
+//  Copyright © 2017 Michael Le. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+  var title: String?
+  var coordinate: CLLocationCoordinate2D
+  var info: String
+  
+  init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+    self.title = title
+    self.coordinate = coordinate
+    self.info = info
+  }
+}
